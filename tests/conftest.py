52457b2935438519be88b4f5d85a88d0010bd4e4
Jumper303 <peter.dobrosi@gmail.com>
2022-05-05 18:00:44 +0200

    Add driver options

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import json
 from selenium import webdriver
+from os.path import exists
 
 CONFIG_PATH = "../config.json"
 
@@ -12,8 +13,15 @@ def config():
 @pytest.fixture(autouse=True, scope='function')
 def setup(request, config):
     # before test
-    request.cls.driver = webdriver.Chrome(config["chromedriver"])
-    request.cls.config = config
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    if config["headless"]:
+        options.add_argument("--headless")
+    if "chromedriver" in config and exists(config["chromedriver"]):
+        request.cls.driver = webdriver.Chrome(config["chromedriver"], options=options)
+        request.cls.config = config
+    else:
+        raise Exception("chromedriver location is not set or not found")
     yield
     # after test
     request.cls.driver.close()
